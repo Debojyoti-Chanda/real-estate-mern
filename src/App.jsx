303d0c9cad34b3c70ab8f3266bcc121c93dc0493
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2024-02-25 01:01:10 +0530

    Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
 
 /**
  * @description layout or the landing page is created 
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <>
+      <Header/>
       <div className='text-3xl font-bold underline'>App</div>
       <Outlet/>
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import searchicon from "../assets/searchicon.svg";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4 flex flex-col md:flex-row justify-between items-center">
+      {/* Logo */}
+      <Link to="/">
+        <div className="flex items-center mb-1 md:mb-0">
+          {/* <img src="logo.png" alt="Logo" className="h-8 mr-4" /> */}
+          <h1 className="text-xl font-bold">Magic Estate</h1>
+        </div>
+      </Link>
+      {/* Search Bar */}
+      <form className="flex items-center justify-center flex-grow m-1 md:mt-0 bg-gray-700 rounded-xl max-w-64">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="bg-transparent p-2 min-w-52 focus:outline-none"
+        />
+        <button type="submit">
+          <img src={searchicon} alt="search icon" className="h-8 w-8" />
+        </button>
+      </form>
+
+      {/* Navigation Links */}
+      <nav className="flex flex-col md:flex-row items-center">
+        <Link
+          to="/"
+          className="block md:inline-block text-center md:text-left mb-1 md:mb-0 md:mr-4 hover:underline"
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="block md:inline-block text-center md:text-left mb-1 md:mb-0 md:mr-4 hover:underline"
+        >
+          About
+        </Link>
+        <Link
+          to="/login"
+          className="block md:inline-block text-center md:text-left hover:underline"
+        >
+          Login
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
