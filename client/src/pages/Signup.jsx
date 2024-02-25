1bd329a1d4c7c19e39909e50e2d0ce091748da11
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2024-02-25 16:34:13 +0530

    Create Signup page in client

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <>
+      <section className="bg-gray-300">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a
+            href="#"
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-950"
+          >
+            Sign-Up
+          </a>
+          <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-600 border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
+                Create and account
+              </h1>
+              {/* form  */}
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block mb-2 text-sm font-medium text-white"
+                  >
+                    Your Username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="border   sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Username"
+                    required=""
+                  />
+                </div>
 
-export default Signup
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required=""
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-slate-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Create an account
+                </button>
+              </form>
+
+              <p className="text-sm font-light text-gray-200">
+                  Already have an account?{" "}
+                  <Link
+                  to={"/login"}
+                    className="font-medium hover:underline text-primary-500"
+                  >
+                    Login here
+                  </Link>
+              </p>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Signup;
