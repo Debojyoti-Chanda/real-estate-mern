211336b8fae55995bcbbed8916fedde6aae32afe
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2024-02-25 12:16:30 +0530

    Add Test API route and controller

---
+++ api/controller/user.controller.js
@@ -0,0 +1,3 @@
+module.exports.test = (req, res, next) => {
+    res.json({ message: "Test API route is working!!" });
+}
--- api/index.js
+++ api/index.js
@@ -5,14 +5,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
 
+const userRouter = require("./routes/user.route");
+
 app.use(cors())
 
 dotenv.config(); //This will load the variables from your .env file into process.env
 
 // Body parser middleware
 app.use(bodyParser.json());
-
-
 
 // Connect to MongoDB
 mongoose
@@ -21,6 +21,11 @@ mongoose
     console.log("MongoDB connected");
   })
   .catch((err) => console.log(err));
+
+
+// Routes
+app.use("/api/user", userRouter);
+
 
 //App listening
 const PORT = process.env.PORT || 3000;
