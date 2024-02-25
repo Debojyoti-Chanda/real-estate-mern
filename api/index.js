ff417b8be82f4eb65e9a96d15b40511d2cc63ea8
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2024-02-25 11:41:53 +0530

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,29 @@
 const express = require("express");
-
+var cors = require('cors')
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 
+app.use(cors())
 
-app.listen(3000, () => {
-    console.log("Server is listening aat port 3000");
+dotenv.config(); //This will load the variables from your .env file into process.env
+
+// Body parser middleware
+app.use(bodyParser.json());
+
+
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log(err));
+
+//App listening
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening at port ${PORT}`);
 })
