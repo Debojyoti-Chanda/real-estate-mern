2db2d770ab406802a554ed614a986ca3c9bfa18f
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2024-02-25 15:26:21 +0530

    create signup api route

---
+++ api/controller/auth.controller.js
@@ -0,0 +1,19 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+
+
+module.exports.postSignup = (req, res, next) => {
+  const { username, email, password } = req.body;
+  return bcrypt
+    .hash(password, 12)
+    .then((hashp) => {
+      const newUser = new userModel({ username, email, password: hashp });
+      return newUser.save();
+    })
+    .then((usrObj) => {
+      res.status(201).json({ message: "Welcome!!" });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error during saving user" , error : err.message});
+    });
+};
