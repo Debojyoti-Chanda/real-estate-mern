4d7f5ff25b4c5481be693928c84babdf166d14c5
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2024-03-08 00:17:58 +0530

    Fix footer display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 /**
- * @description layout or the landing page is created 
- * @returns 
+ * @description layout or the landing page is created
+ * @returns
  */
 function App() {
-
-
   return (
     <>
-      <Header/>
-      {/* <div className='text-3xl font-bold underline'>App</div> */}
-      <Outlet />
-      <Footer/>
+      <div className="flex flex-col min-h-screen justify-between">
+        <Header />
+        {/* <div className='text-3xl font-bold underline'>App</div> */}
+        <Outlet />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
